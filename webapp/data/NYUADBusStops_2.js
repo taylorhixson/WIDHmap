a78918cb11a0f9c0c07817720fffeaa63e718204
Taylor H <taylorhixson@gmail.com>
2019-10-09 14:27:16 +0400

    Update data, edit index.html

--- webapp/data/NYUADBusStops_2.js
+++ webapp/data/NYUADBusStops_2.js
@@ -3,16 +3,16 @@ var json_NYUADBusStops_2 = {
 "name": "NYUADBusStops_2",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Name": "Stop 1", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.4336587, 24.5232396 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 3", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.4237088, 24.5331109 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 4", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.4194145, 24.5328736 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 5", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.3741557, 24.497664 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 6", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.3704708, 24.4950483 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 8", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.364605, 24.4874152 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 7", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.3640728, 24.4906074 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 9", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.3665316, 24.4841394 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 11", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.3773787, 24.4705078 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 10", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.3720216, 24.4764561 ] } },
-{ "type": "Feature", "properties": { "Name": "Stop 2", "description": null }, "geometry": { "type": "Point", "coordinates": [ 54.431841, 24.5250058 ] } }
+{ "type": "Feature", "properties": { "Name": "Stop 1", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.4336587, 24.5232396 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 3", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.4237088, 24.5331109 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 4", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.4194145, 24.5328736 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 5", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.3741557, 24.497664 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 6", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.3704708, 24.4950483 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 8", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.364605, 24.4874152 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 7", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.3640728, 24.4906074 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 9", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.3665316, 24.4841394 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 11", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.3773787, 24.4705078 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 10", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.3720216, 24.4764561 ] } },
+{ "type": "Feature", "properties": { "Name": "Stop 2", "description": "A Hafilat card (bus pass) is required to board the bus. Purchase one at the airport, a vending machine in the city, or LuLu supermarket." }, "geometry": { "type": "Point", "coordinates": [ 54.431841, 24.5250058 ] } }
 ]
 }
